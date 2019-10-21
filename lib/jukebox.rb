f8ac6ed1c10634bb90dcdf001d3fedbd2311da08
Kyle Cole <kylepcole@gmail.com>
2019-10-21 21:48:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,7 +22,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip.to_i
   
-  song_id = input.is_a? Integer ? input-1 : songs.find_index(input)
+  song_id = input.is_a?Integer ? input-1 : songs.find_index(input)
   
   if !songs.include?(input)
     "Invalid input, please try again"
