9edce004f67376fa52e350eea24f44e0415e489b
Kyle Cole <kylepcole@gmail.com>
2019-10-21 21:32:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  
+  puts input.is_an_int
   song_id = input.is_an_int ? input-1 : songs.find_index(input)
   
   if !songs.include?(strip)
